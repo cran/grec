9a5670ba93a4328f5e17a662940d85d550941212
Wencheng Lau-Medrano <luis.laum@gmail.com>
2020-02-04 16:20:02 +0000

    version 1.3.2

--- R/Raster-class.R
+++ R/Raster-class.R
@@ -3,11 +3,13 @@
 #' @export
 detectFronts.RasterLayer <- function(x, method = "BelkinOReilly2009", intermediate = FALSE, ...){
 
+  checkArgs_df_RasterLayer(x = x)
+
   # Extract coordinates and data for calculate fronts from Raster and convert to list
   startMatrix <- values(x)
   startMatrix <- list(x = seq(from = x@extent@xmin, to = x@extent@xmax, length.out = x@ncols),
                       y = seq(from = x@extent@ymin, to = x@extent@ymax, length.out = x@nrows),
-                      z = startMatrix)
+                      z = matrix(data = startMatrix, nrow = x@ncols))
 
   allOuts <- detectFronts(x = startMatrix, method = method, intermediate = intermediate, ...)
 
